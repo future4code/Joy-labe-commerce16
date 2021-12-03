602b0584033a68c6b67ba774553a75b6fdf9a180
unknown <victorsantos732@gmail.com>
2021-12-02 21:38:18 -0300

    Terminei o filtro

--- src/components/ContainerDeProdutos.jsx
+++ src/components/ContainerDeProdutos.jsx
@@ -8,18 +8,56 @@ const ContainerProdutos = styled.div`
     grid-template-columns: 1fr 1fr 1fr;
     flex-wrap: wrap;
 `
+const ContainerHeader = styled.div`
+
+`
 
 class ContainerDeProdutos extends React.Component {
+    state = {
+        ordenar:""
+    }
+
+    filtrarProdutos = () => {
+        return this.props.produtos
+        .filter((produto) => {
+            return this.props.minFilter === "" || produto.valor >= this.props.minFilter
+        })
+        .filter((produto) => {
+            return this.props.maxFilter === "" || produto.valor <= this.props.maxFilter
+        })
+        .filter((produto) => {
+            return produto.titulo.toLowerCase().includes(this.props.nomeFilter.toLowerCase())
+        })
+        .sort((a, b)=> this.state.ordenar === "Crescente" ? a.valor - b.valor : b.valor - a.price)
+    }
+
+    onChangeOrdenar = (e) =>{
+        this.setState({ordenar: e.target.value})
+    }
+
     render(){
-        const arrayProdutos = this.props.produtos
+        const arrayProdutos = this.filtrarProdutos()
         return(
-            <ContainerProdutos>
-                {arrayProdutos.map((produto)=>{
-                    return(
-                        <Produto prod={produto}/>
-                    )
-                })}            
-            </ContainerProdutos>
+            <div>
+                <ContainerHeader>
+                    <p>Quantidade de produtos: {arrayProdutos.length}</p>
+                    <nav>
+                    Ordenação:
+                    <select value={this.state.ordenar} onChange={this.onChangeOrdenar}>
+                        <option value={"Crescente"}>Crescente</option>
+                        <option value={"Decrescente"}>Decrescente</option>
+                    </select>
+                    </nav>
+                </ContainerHeader>
+                <ContainerProdutos>
+                    {arrayProdutos.map((produto)=>{
+                            return(
+                                <Produto prod={produto}/>
+                            )
+                        })
+                    }            
+                </ContainerProdutos>
+            </div>
         )
     }
 }
